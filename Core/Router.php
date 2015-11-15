05378b7d9c3319a9ca2504f3cbc16229d47b896a
Stefan <steev90@gmail.com>
2015-11-15 11:49:05 +0200

    first commit

---
+++ Core/Router.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Atanasov
+ * Date: 11/15/15
+ * Time: 11:13 AM
+ */
+
+namespace Core;
+
+
+class Router {
+    public function get()
+    {
+        echo 'some';
+    }
+} 
---
+++ include.inc.php
@@ -0,0 +1,6 @@
+<?php
+/**
+ * @var Composer\Autoload\ClassLoader $oLoader
+ */
+$oLoader = require __DIR__ . '/vendor/autoload.php';
+$oLoader->add('Core', __DIR__);
